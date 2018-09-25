3a1715a83f55cf754b9a068c4a01c47602546312
ishancoder <isaxena171@gmail.com>
2018-09-26 00:22:27 +0530

    added comments in questions and exams route files

--- routes/exams.js
+++ routes/exams.js
@@ -1,6 +1,10 @@
 import express from 'express';
 const router = express.Router();
 
+/*
+This files contains all the routes after /exams. Any APIs regarding exams will be written here.
+For Example: get API for getting exams details and format etc.
+*/
 router.get('/', (req, res) => {
     res.send("<h1>Welcome to exams router</h1>");
 });
--- routes/questions.js
+++ routes/questions.js
@@ -1,6 +1,10 @@
 import express from 'express';
 const router = express.Router();
 
+/*
+This files contains all the routes after /questions. Any APIs regarding questions will be written here.
+For Example: get API for getting questions body etc.
+*/
 router.get('/', (req, res) => {
     return res.send('<h1>Welcome to questions router</h1>');
 })
